6d30836f0c8da2759e46dd6b649792e64f25b042
cfilipecruz <marques6832@gmail.com>
2024-06-08 10:23:16 +0100

    ex_004

---
+++ Exercises/ex_004.py
@@ -0,0 +1,23 @@
+value = input('Type a value: ')
+
+#Verify wich type its the variable
+print("The variable is always a", type(value))
+# Verify if the variable is only text
+print("Value is a string?", value.isalpha())
+# Verify if the variable is only numbers
+print("Value is a number?", value.isnumeric())
+# Verify if the variable as text or numbers
+print("Value is only numbers?", value.isalnum())
+# Verify if the variable is only capital letters
+print("Value is only capital case?", value.isupper())
+# Verify if the variable is only lowercase letters
+print("Value is only lower case?", value.islower())
+# Verify if the variable can be a name of a variable falseex:1teste
+print("Value is only punctuation?", value.isidentifier())
+# Verify if the variable is printable falseex: teste\nteste
+print("Value is only punctuation characters?", value.isprintable())
+# Verify if the variable is only spaces
+print("Value is only white space?", value.isspace())
+
+
+
