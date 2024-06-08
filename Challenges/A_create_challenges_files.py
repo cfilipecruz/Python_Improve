33d01c8728865962a6de12fe34f95a4ada962338
cfilipecruz <marques6832@gmail.com>
2024-06-08 11:20:36 +0100

    New folder challenges +  some exercises done

--- Challenges/A_create_challenges_files.py
+++ Challenges/A_create_challenges_files.py
@@ -1,6 +1,6 @@
 # Create files named ex_005 to ex_100
-for i in range(7, 20):
-    file_name = f"ex_{i:03d}.py"
+for i in range(5, 50):
+    file_name = f"challenges_{i:03d}.py"
     with open(file_name, 'w') as file:
         pass  # Create an empty file
 
---
+++ Course/A_create_classes_files.py
@@ -0,0 +1,7 @@
+# Create files named ex_005 to ex_100
+for i in range(7, 20):
+    file_name = f"class_{i:03d}.py"
+    with open(file_name, 'w') as file:
+        pass  # Create an empty file
+
+print("Files created successfully.")
