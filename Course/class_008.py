cd8fb5e0f051b800733ce8b22d8466aa10b84123
cfilipecruz <marques6832@gmail.com>
2024-06-10 12:44:04 +0100

    MorNew class

---
+++ Course/class_008.py
@@ -0,0 +1,11 @@
+from math import sqrt
+from math import ceil
+import emoji
+
+#num = int(input('Type a number: '))
+#square = sqrt(num)
+#Round up
+#print('The square root of {} is {}'.format(num, ceil(square)))
+
+print(emoji.emojize('Python is :thumbs_up:'))
+
