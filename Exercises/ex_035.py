9e0517a748a2e7910c8beca4627d02573f23b98c
cfilipecruz <marques6832@gmail.com>
2024-06-14 23:08:38 +0100

    More exercises

---
+++ Exercises/ex_035.py
@@ -0,0 +1,11 @@
+side_01 = int(input('Type the first side: '))
+side_02 = int(input('Type the second side: '))
+side_03 = int(input('Type the third side: '))
+total_01_02 = side_01 + side_02
+total_02_03 = side_02 + side_03
+total_03_01 = side_03 + side_01
+
+if total_01_02 > side_03 and total_02_03 > side_01 and total_03_01 > side_02:
+    print('The values {}, {} and {} form a triangle.'.format(side_01, side_02, side_03))
+else:
+    print('The values {}, {} and {} don´t form a triangle'.format(side_01, side_02, side_03))
