98acbb1d32664df96795466abb2f9b78e548cc38
cfilipecruz <marques6832@gmail.com>
2024-06-19 11:28:12 +0100

    Studying....

---
+++ Course/class_010.py
@@ -0,0 +1,5 @@
+name = str(input('Qual o seu nome: '))
+if 'Filipe' in name:
+    print('Correct name')
+else:
+    print('Nome invalido')
--- Course/class_011.py
+++ Course/class_011.py
@@ -1,5 +1,18 @@
-name = str(input('Qual o seu nome: '))
-if 'Filipe' in name:
-    print('Correct name')
-else:
-    print('Nome invalido')
+a = 5
+b = 6
+
+print('The values are \033[1;33m{}\033[m and \033[1;36m{}\033[m'.format(a, b))
+
+#or
+
+name = 'Filipe'
+
+print('Pleasure to meet you {}{}{}'.format('\033[4;31m',name,'\033[m'))
+
+#or
+cores={'redBold':'\033[1;31m',
+       'clean':'\033[m',
+       'blackandwhite':'\033[1;0;40m',
+       }
+
+print('teste {a}')
